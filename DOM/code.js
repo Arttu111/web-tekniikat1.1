65e91b1b032153c76b9c170ae0b67c945ce09653
Arttu Kuismin <117356548+Arttu111@users.noreply.github.com>
2022-12-01 10:21:07 +0200

    Add files via upload

---
+++ DOM/code.js
@@ -0,0 +1,16 @@
+let divs = document.querySelectorAll("div");
+
+/** @type Element */
+let div;
+
+for (div of divs) {
+    console.log(div.textContent);
+    
+}
+
+let listItems = document.querySelectorAll("ul>li");
+
+for (li of listItems) {
+    li.textContent= "John Doe"
+    
+}
